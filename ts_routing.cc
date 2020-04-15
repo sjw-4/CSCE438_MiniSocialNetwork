9b484d75639655094ff8ccbf2bba6b383f276405
SJ <sj.wilkins.4@gmail.com>
2020-04-15 08:30:13 -0500

    big oof x3

--- ts_routing.cc
+++ ts_routing.cc
@@ -18,14 +18,14 @@ using tinysocial::User;
 using tinysocial::ReplyStatus;
 using tinysocial::Post;
 using tinysocial::NewPost;
-using tinysocial::ServerInfo
+using tinysocial::ServerInfo;
 using tinysocial::TinySocial;
 
 struct IPInfo {
     std::string ipAddress;
     std::string portNo;
     bool alive;
-}
+};
 
 class TinySocialRouting final : public TinySocial::Service {
 private:
@@ -33,24 +33,24 @@ private:
     IPInfo curMaster;
 public:
     Status ServerLogin(ServerContext* context, const ReplyStatus* msgStat, ReplyStatus* replyStat) override {
-        if(msgStat != "0") {
+        /*if(msgStat != "0") {
             std::cout << "routing::ServerLogin::Error" << std::endl;
             return Status::OK;
-        }
+        }*/
         std::string serverInfo = context->peer();
         std::cout << "Connected peer address is: " << serverInfo << std::endl;
         //TODO set curMaster if none is set
         return Status::OK;
     }
-    Status GetServerInfo(ServerCOntext* context, const User* user, ServerInfo* si) override {
+    Status GetServerInfo(ServerContext* context, const User* user, ServerInfo* si) override {
         //TODO
-        si = curMaster;
+        //si = curMaster;
         return Status::OK;
     }
 };
 
 void runServer(std::string serverAddr) {
-    TinySocialImpl service;
+    TinySocialRouting service;
 
     ServerBuilder builder;
     builder.AddListeningPort(serverAddr, grpc::InsecureServerCredentials());
