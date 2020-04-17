e134d55635556517ac84c1679febf4dd20ca26e2
AlricLobo <39924933+AlricLobo@users.noreply.github.com>
2020-04-16 23:51:33 -0500

    added outputs

--- bootscript.sh
+++ bootscript.sh
@@ -3,10 +3,12 @@
 
 if [ "$1" == "ts_server" ]
 then
+	echo "Server is booting up..."
 	./ts_server -p $2 -h $3 -r $4 	
 fi
 
 if [ "$1" == "ts_routing" ]
 then
+	echo "Router is booting up..."
 	./ts_routing $2
 fi
